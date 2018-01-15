7304165da15c7f7a6071de5a18e4d898154d4e5f
王宏宇 <wanghongyu@chuchujie.com>
2018-01-16 00:35:17 +0800

    2018.01.16 测试salt返回full

--- saltapi/models.py
+++ saltapi/models.py
@@ -6,14 +6,7 @@ from django.db import models
 # Create your models here.
 
 
-class accect_cmd(models.Model):
-    command = models.CharField(max_length=50, unique=True, verbose_name=u'命令')
-    status = models.CharField(max_length=20, verbose_name=u'状态')
-    def __unicode__(self):
-        return u'{0} {1}'.format(self.command, self.status)
-
-
-class SaltReturns(models.Model):
+class SaltReturns_info(models.Model):
     fun = models.CharField(max_length=50)
     jid = models.CharField(max_length=255)
     return_field = models.TextField(db_column='return')
@@ -25,10 +18,3 @@ class SaltReturns(models.Model):
         db_table = 'salt_returns'
     def __unicode__(self):
         return u'%s %s %s' % (self.jid, self.id, self.return_field)
-
-
-class record(models.Model):
-    time = models.DateTimeField(u'时间', auto_now_add=True)
-    comment = models.CharField(max_length=128, blank=True, default='', null=True, verbose_name=u"记录")
-    def __unicode__(self):
-        return u'%s %s' % (self.time, self.comment)
--- workorder/models.py
+++ workorder/models.py
@@ -4,3 +4,30 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class WorkOrder_Host_info(models.Model):
+    apply_choices = (
+        (),
+        ()
+    )
+    submit_time = models.DateField(auto_now_add=True)                   # 提交时间
+    submit_user = models.CharField(max_length=20)                       # 提交用户
+    apply_type = models.CharField(max_length=30, choices=apply_choices) # 申请主机类型
+    cloud_type = models.CharField(max_length=30)                        # 云主机类型
+    host_type = models.CharField(max_length=30)                         # 主机类型
+    pubipaddr = models.CharField(max_length=30)                         # 是否要求外网IP
+    serverline = models.CharField(max_length=30)                        # 业务线
+    monitor_url = models.CharField(max_lenth=30)                        # 需监控url
+    git_code = models.CharField(max_lenth=30)                           # git仓库
+    remark = models.CharField(max_length=30)                            # 备注
+    describe = models.CharField(max_length=30)                          # 描述
+    app_leader = models.CharField(max_length=30)                        # app负责人
+
+
+class WorkOrder_Serverline_info(models.Model):
+    submit_time = models.DateField(auto_now_add=True)                   # 提交时间
+    submit_user = models.CharField(max_length=20)                       # 提交用户
+    serverline = models.CharField(max_length=30)                        # 业务线
+    describe = models.CharField(max_length=30)                          # 描述
+    serverline_leader = models.CharField(max_length=30)                 # 业务线负责人
