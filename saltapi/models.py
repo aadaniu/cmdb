2ad86c52c8d4e8e357b4156ef0327c86b9b18f63
wanghongyu <wanghongyu@chuchujie.com>
2018-01-22 14:07:54 +0800

    2018.01.22 model hostgroup

--- saltapi/models.py
+++ saltapi/models.py
@@ -7,6 +7,9 @@ from django.db import models
 
 
 class SaltReturns_info(models.Model):
+    """
+        salt 执行结果返回值
+    """
     exec_time = models.DateTimeField()
     tgt = models.CharField(max_length=100)
     fun = models.CharField(max_length=50)
@@ -18,3 +21,13 @@ class SaltReturns_info(models.Model):
     #     db_table = 'salt_returns'
     def __unicode__(self):
         return u'%s %s %s %s' % (self.tgt_type, self.tgt, self.fun, self.str_kwarg)
+
+
+class HostGroup(models.Model):
+    """
+        自定义主机组，serverline自动创建主机组
+    """
+    group_name = models.CharField(max_length=30)
+    host_info = models.ManyToManyField('opmanage.host_info', through='HostToHostgroup')
+    def __unicode__(self):
+        return self.group_name
